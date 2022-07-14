1c708572e03e055e91366a15152aea8062fa2201
Shubham <shubham.nalawade03122000@gmail.com>
2022-07-14 18:21:45 +0530

    ad

--- src/tests.java
+++ src/tests.java
@@ -2,7 +2,7 @@ public class tests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World122");
+		System.out.println("Hello World1222");
 	}
 
 }
