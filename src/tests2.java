fdc1e106d8c185402a095a2448ab617bebba2964
Shubham <shubham.nalawade03122000@gmail.com>
2022-07-15 11:09:54 +0530

    new commit

--- src/tests2.java
+++ src/tests2.java
@@ -1,7 +1,7 @@
-public class tests {
+public class tests2{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World1222    Tests 1");
+		System.out.println("Hello World1222   Tests 2");
 	}
 }
